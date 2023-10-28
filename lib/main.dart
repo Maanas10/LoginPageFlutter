fe9c6ab5de3cf93fc3ee417c5f4657ed7001009a
maanas <maanaspk@gmail.com>
2023-10-28 15:41:33 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+runApp(MaterialApp(Home:ProfileApp));
+  
+  
+}
+class ProfileApp extends StatefulWidget {
+  const ProfileApp({super.key});
+
+  @override
+  State<ProfileApp> createState() => _ProfileAppState();
+}
+
+class _ProfileAppState extends State<ProfileApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
