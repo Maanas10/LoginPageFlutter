d6d80240329e27b8170628bfc41ec324b676ca26
maanas <maanaspk@gmail.com>
2023-11-04 15:45:02 +0530

    updated

--- lib/screens/log.dart
+++ lib/screens/log.dart
@@ -16,7 +16,49 @@ class _Log_ScreenState extends State<Log_Screen> {
         backgroundColor: Colors.indigo,
       ),
       body: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(alignment: Alignment.center),
+          CircleAvatar(
+            backgroundImage: AssetImage('images/icon2.png'),
+            radius: 70,
+          ),
+          Text(
+            'Maanas PK',
+            style: TextStyle(
+                color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold),
+          ),
+          Text('FLUTTER DEVELOPER ',
+              style: TextStyle(
+                  color: Colors.indigo,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold)),
+          Divider(
+            height: 20,
+            color: Colors.white,
+            indent: 100,
+            endIndent: 120,
+            thickness: 3,
+          ),
+          Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: ListTile(
+                leading: Icon(Icons.phone),
+                title: Text("9020112197"),
+              )),
+          SizedBox(height: 10,),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 40),
+            child: ListTile(
+              leading: Icon(Icons.email),
+              title: Text('maanaspk@gmail.com'),
+            ),
+          )
+        ],
       ),
       backgroundColor: Colors.cyan,
     );
